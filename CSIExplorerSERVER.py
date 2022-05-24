71e4d79531ad01c1eeeaf3e69c68b5ac8d9435e5
alexv <ugarte_alex@discente.ufg.br>
2022-05-24 18:08:58 -0300

    Nem sei mais

--- CSIExplorerSERVER.py
+++ CSIExplorerSERVER.py
@@ -9,37 +9,37 @@ remove_null_subcarriers = False
 remove_pilot_subcarriers = False
 bandwidth = int(input("Qual a largura de banda a ser usada? "))
 
-TCP_IP = "192.168.0.3"
+TCP_IP = "192.168.0.179"
 TCP_PORT = 5501
-BUFFER_SIZE = 512*4 + 18  # 34 surge do NEXMON metadado + packet header
+BUFFER_SIZE = 512*4  # 34 surge do NEXMON metadado + packet header
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind((TCP_IP, TCP_PORT))
-print("Waiting for connections...")
-s.listen(1)
-conn, addr = s.accept()
-print('Connected by', addr)
-print("\n\n")
-time.sleep(1)
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((TCP_IP, TCP_PORT))
+    print("Waiting for connections...")
+    s.listen(1)
+    conn, addr = s.accept()
+    print('Connected by', addr)
+    print("\n\n")
+    time.sleep(1)
 
-plotter = Plotter(bandwidth)
+    plotter = Plotter(bandwidth)
 
-n_frame = 0
-while True:
-    # recebendo os frames
-    frame = conn.recv(BUFFER_SIZE)
+    n_frame = 0
+    while True:
+        # recebendo os frames
+        frame = conn.recv(BUFFER_SIZE)
 
-    # processando as informacoes do pacote CSI
-    frame_info = decoder.read_frame(frame, bandwidth)
-    frame_info.print(0, n_frame=n_frame)
+        # processando as informacoes do pacote CSI
+        frame_info = decoder.read_frame(frame, bandwidth)
+        frame_info.print(0, n_frame=n_frame)
 
-    # Lendo o CSI de fato
-    csi = frame_info.get_csi(
-        0,
-        remove_null_subcarriers,
-        remove_pilot_subcarriers
-    )
+        # Lendo o CSI de fato
+        csi = frame_info.get_csi(
+            0,
+            remove_null_subcarriers,
+            remove_pilot_subcarriers
+        )
 
-    plotter.update(csi)
-    n_frame += 1
-    del frame_info
+        plotter.update(csi)
+        n_frame += 1
+        del frame_info
--- ClientPi.py
+++ ClientPi.py
@@ -5,23 +5,22 @@ UDP_IP = "255.255.255.255"  # SEGUINDO O GITHUB DO NEXMON (SOURCE 10.10.10.10)
 UDP_PORT = 5500
 
 # preparando server do csi
-sock = socket.socket(socket.AF_INET,  # Internet
-                     socket.SOCK_DGRAM)  # UDP
+with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as sock:  # UDP
 
-sock.bind((UDP_IP, UDP_PORT))
+    sock.bind((UDP_IP, UDP_PORT))
 
-# conectando no server do PC
-server_pc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_pc.connect(('192.168.0.3', 5501))
+    # conectando no server do PC
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_pc:
+        server_pc.connect(('192.168.0.179', 5501))
 
-while True:
-    try:
-        data, addr = sock.recvfrom(512 * 4 + 18)  # buffer size is 2048 + 18 bytes
-        server_pc.send(data)
+        while True:
+            try:
+                data, addr = sock.recvfrom(512 * 4)  # buffer size is 2048 + 18 bytes
+                server_pc.sendall(data)
 
-    except ConnectionError as e:
-        decision = input("Servidor caiu. Reconectar? ([y]/n) ")
-        if decision in ["n", "N"]:
-            exit(0)
-        else:
-            continue
+            except ConnectionError as e:
+                decision = input("Servidor caiu. Reconectar? ([y]/n) ")
+                if decision in ["n", "N"]:
+                    exit(0)
+                else:
+                    continue
